0eff2d0e1d10f0b794dc0936d4efeb3cc09d5241
jongmini <jongmin.choi@gmail.com>
2014-02-19 10:47:24 -0800

    added more images. src info clears

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,25 +1,28 @@
-// $(document).ready(function(){
-//     var i = Math.floor(Math.random()*images.length);
-//     var context = {img: images[i]};
-//     var $show_image = $(HandlebarsTemplates.template(context));
-//     $('#images').append($show_image);
-//   });
 
 
 $(function(){
   var images=[
-  "https://github-camo.global.ssl.fastly.net/0634f55ffcb56c8e43b0b84893ce06f3ca3fb89c/687474703a2f2f36363661363538633632346133633033613662322d32356364613035396439373564326633313863303365393062636631376334302e7239322e6366312e7261636b63646e2e636f6d2f756e73706c6173685f353264356262656638613631335f312e4a5047",
-  "https://github-camo.global.ssl.fastly.net/1957d530873109df90a52077f098c17b09ec5d90/687474703a2f2f36363661363538633632346133633033613662322d32356364613035396439373564326633313863303365393062636631376334302e7239322e6366312e7261636b63646e2e636f6d2f756e73706c6173685f353235303961666162613762315f312e4a5047",
-  "https://github-camo.global.ssl.fastly.net/1fec18fa9c02a6607519bcadb880d2b5e87c0e91/687474703a2f2f36363661363538633632346133633033613662322d32356364613035396439373564326633313863303365393062636631376334302e7239322e6366312e7261636b63646e2e636f6d2f756e73706c6173685f353263326432616164313537365f312e4a5047"
+  "http://31.media.tumblr.com/78ce1831f575f06a6ca966ee2c9198f1/tumblr_n10nb0TY4u1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/ad80d557aee5736d7b01ca13846781f5/tumblr_n10n5ygMJI1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/8fb80a2a6111ffbf05defc3a758a2f8a/tumblr_n10n4rpjvZ1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/d4955c5fb31743bd0740f5001adafb79/tumblr_n10n3wfcrl1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/350cbf6444f32fadff401d12c318cdbd/tumblr_n10n2x71Dz1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/18e25cdcdaaced3b2b8a467724720ece/tumblr_n10n1wmxiS1st5lhmo1_1280.jpg",
+  "http://24.media.tumblr.com/afe9f67b4ec353bce470203300f68618/tumblr_n10mzgcbTV1st5lhmo1_1280.jpg",
+  "http://24.media.tumblr.com/93a48760402963e177485f3af78240e0/tumblr_n10mycKEv41st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/f4f22b2eb234e51e3183fde267ccdc70/tumblr_n10mxm8L7u1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/738750bb061d484aad112a89548672d5/tumblr_n10mwz4NQo1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/b96eedd08ac55d61bae0534d812a9ee7/tumblr_n0hq1kxq7j1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/712c985b8c18226a0d394eb90b83e94b/tumblr_n0hq0nxdQd1st5lhmo1_1280.jpg",
+  "http://25.media.tumblr.com/d55a509993790027240311c9f611aaf8/tumblr_n0hpzpKEfE1st5lhmo1_1280.jpg",
+  "http://31.media.tumblr.com/7eaaac07f23f03cb51a296bd81d77569/tumblr_n0hpyugseU1st5lhmo1_1280.jpg",
+  "http://24.media.tumblr.com/f87b54bbce49e59debf7606662f54862/tumblr_n0hpxxDOJ91st5lhmo1_1280.jpg"
   ];
 
-  // for (var i=0;i<images.length;i++){
-  //   var context = {img: images[i]};
-  //   var $show_image = $(HandlebarsTemplates.template(context));
-  //   $('#images').append($show_image);
-  // }
+  var length = images.length;
 
-    var i = Math.floor(Math.random()*images.length);
+
+    var i = Math.floor(Math.random()*length);
     var context = {img: images[i]};
     var $show_image = $(HandlebarsTemplates.template(context));
     $('#images').append($show_image);
@@ -29,25 +32,21 @@ $(function(){
 
   var src = $('#images div img').attr('src');
   var $show_src = $(HandlebarsTemplates.src({src: src}));
+  $('#displayLink').empty();
   $('#displayLink').append($show_src);
 
   });
 
   $('#shuffleImage').click(function(){
-    // $('#images').remove();
 
-    var i = Math.floor(Math.random()*images.length);
-    var context = {img: images[i]};
-    var $show_image = $(HandlebarsTemplates.template(context));
-
-    $('#images').append($show_image);
-
-    // $("#images").attr('src', $show_image);
+    var i = Math.floor(Math.random()*length);
+    // var context = {img: images[i]};
+    // var $show_image = $(HandlebarsTemplates.template(context));
+    // $('#images').append($show_image);
+    $('#displayLink').empty();
+    $('#images div img').attr('src', images[i]);
 
   });
-
-
-
 
 
 });
